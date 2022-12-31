3896ce086cad4db75767d505491986b155b1142b
Mihai Smarandache <mihai.sanfran@gmail.com>
2022-12-30 17:57:25 -0700

    Export type from reducers function

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,3 +1,4 @@
 // export everything that you need access to in project
 export * from "./store";
 export * as actionCreators from "./action-creators";
+export * from "./reducers";
--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -6,3 +6,7 @@ const reducers = combineReducers({
 });
 
 export default reducers;
+
+// ReturnType is a built in helper inside of typescript
+// Takes in reducers function and gives back the type of whatever the function returns and assign it to RootState
+export type RootState = ReturnType<typeof reducers>;
